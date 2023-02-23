0ebd3eed17382e13691c01676e8741097495d8b6
Pablo Nunez <polnunez@outlook.com>
2023-02-23 11:57:43 -0300

    Directorio de environments modificado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  rickAndMortyApi: {
+    baseUrl: 'https://rickandmortyapi.com/api'
+  },
+  authApi: {
+    baseUrl: 'https://api-auth-moby.herokuapp.com/api/user'
+  }
+};
